fb45e06beafcbf61fe1b399b54949b525ded3f5f
Artem-1984 <118545099+Artem-1984@users.noreply.github.com>
2024-07-24 20:39:07 +0300

    Add files via upload

---
+++ prodect/asgi.py
@@ -0,0 +1,14 @@
+"""
+ASGI config for prodect project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'prodect.settings')
